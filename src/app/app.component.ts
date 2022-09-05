cea9c6a38c4efd08e35ef10a067ed8569612bea4
Royer Adames <royeraadames@gmail.com>
2022-09-05 15:21:50 -0400

    handle save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {UntypedFormControl, UntypedFormGroup} from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatDialog} from "@angular/material/dialog";
 import {OnDeleteDialogComponent} from "./on-delete/on-delete-dialog.component";
-import {DocInterface, docs} from './docs'
+import {docs} from './docs'
+import {Subscription} from "rxjs";
+import {DocModel} from "./model/doc-model";
+import {DocFormModel} from "./model/doc-form-model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   isEdit = true;
-  form = new UntypedFormGroup({
-    themeToggle: new UntypedFormControl(false)
-  })
+  form = new FormGroup<DocFormModel>({
+    themeToggle: new FormControl(false, {nonNullable: true}),
+    title: new FormControl('', {nonNullable: true}),
+    content: new FormControl('', {nonNullable: true}),
+  }, {validators: [Validators.required]});
   docs = docs;
-  currentDoc: DocInterface = this.docs[1]
-
+  currentDoc: DocModel = this.docs[1]
+  formSubscriptions$ = new Subscription();
 
   constructor(public dialog: MatDialog) {
   }
 
-  goToDoc(doc: DocInterface) {
+  goToDoc(doc: DocModel) {
     this.currentDoc = doc;
+    this.form.controls.title.setValue(this.currentDoc.title)
+    this.form.controls.content.setValue(this.currentDoc.content)
+
   }
 
   ngOnInit(): void {
-    this.form.controls['themeToggle'].valueChanges.subscribe(() => {
-      console.log('themeToggle')
-    })
+    this.form.controls.title.setValue(this.currentDoc.title)
+    this.form.controls.content.setValue(this.currentDoc.content)
+    this.formSubscriptions()
   }
 
-
   onDelete() {
-    console.log('onDelete');
     const dialogRef = this.dialog.open(OnDeleteDialogComponent, {
       width: '343px',
     });
@@ -42,6 +46,7 @@ export class AppComponent implements OnInit {
       this.docs = this.docs.filter(doc => doc.id !== this.currentDoc.id)
       //todo: what to do when you delete a doc?
       this.currentDoc = {id: 0, title: '', content: '', date: ''}
+      this.form.patchValue({title: '', content: ''})
     });
   }
 
@@ -50,13 +55,39 @@ export class AppComponent implements OnInit {
   }
 
   onAdd() {
-    console.log('onAdd');
     this.docs.push({
       id: this.docs.length + 1,
-      title: 'New Doc.md',
-      content: '#New Doc Content',
+      title: this.form.value.title! || 'New Doc.md',
+      content: this.form.value.content! || '#New Doc Content',
       date: new Date().toDateString()
     })
     this.currentDoc = this.docs[this.docs.length - 1]
+  }
+
+  onSave() {
+    const currentDocIndex = this.docs.findIndex(doc => doc.id === this.currentDoc.id)
+
+    if (currentDocIndex !== -1) {
+      this.docs.splice(currentDocIndex, 1, this.currentDoc)
+      return
+    }
+
+    this.onAdd()
+  }
+
+  ngOnDestroy(): void {
+    this.formSubscriptions$.unsubscribe()
+  }
+
+  private formSubscriptions() {
+    this.formSubscriptions$.add(this.form.controls.themeToggle.valueChanges.subscribe(() => {
+      console.log('themeToggle')
+    }))
+    this.formSubscriptions$.add(this.form.controls.content.valueChanges.subscribe(value => {
+      this.currentDoc.content = value
+    }))
+    this.formSubscriptions$.add(this.form.controls.title.valueChanges.subscribe(value => {
+      this.currentDoc.title = value
+    }))
   }
 }
